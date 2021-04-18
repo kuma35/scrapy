c9ba8ef269addb2e22637add400f5bfb2fba3870
kuma35 <sindobook@gmail.com>
2021-04-19 07:53:14 +0900

    generalize sphinx-intl-msgmerge.sh
    JP:パス記述を sphinx-intl-msgmerge.sh から追い出して汎用化。
    コマンドライン指定無しの場合にusage表示追加。
    使用例及び、自分の普段使い用に example-sphinx-intl-msgmerge.sh 追加。

---
+++ docs/utils/example-sphinx-intl-msgmerge.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+bash ./sphinx-intl-msgmerge.sh "${HOME%/}/work/docs-ja-scrapy/docs/locale/ja/LC_MESSAGES/" "${HOME%/}/work/scrapy-docs-ja/docs/_build/gettext/" "${HOME%/}/work/scrapy-docs-ja/docs/locale/ja/LC_MESSAGES"
+
--- docs/utils/sphinx-intl-msgmerge.sh
+++ docs/utils/sphinx-intl-msgmerge.sh
@@ -1,12 +1,25 @@
-#! /bin/bash
-SRC_DIR=${HOME}/work/docs-ja-scrapy/docs/locale/ja/LC_MESSAGES/
-DST_POT_DIR=${HOME}/work/scrapy-docs-ja/docs/_build/gettext/
-DST_DIR=${HOME}/work/scrapy-docs-ja/docs/locale/ja/LC_MESSAGES/
+#!/bin/bash
+CMDNAME=`basename $0`
+if [ $# -ne 3 ]; then
+    echo "Usage: $CMDNAME src_po_dir dst_pot_dir dst_po_dir(output_dir)" 1>&2
+    echo "src_po_dir: a_sphinx_folder/locale/ja/LC_MESSAGES" 1>&2
+    echo "dst_pot_dir: now_sphinx_folder/_build/gettext" 1>&2
+    echo "src_po_dir: now_sphinx_folder/locale/ja/LC_MESSAGES" 1>&2
+  exit 1
+fi
+
+
+# SRC_DIR=${HOME%/}/work/docs-ja-scrapy/docs/locale/ja/LC_MESSAGES/
+# DST_POT_DIR=${HOME%/}/work/scrapy-docs-ja/docs/_build/gettext/
+# DST_DIR=${HOME%/}/work/scrapy-docs-ja/docs/locale/ja/LC_MESSAGES/
+SRC_DIR=$1
+DST_POT_DIR=$2
+DST_DIR=$3
 find  ${SRC_DIR} -name "*.po" -printf "%P\n" | while read src_file_name 
 do
     # echo "------------"
     # echo ${SRC_DIR}${src_file_name}
     # echo ${DST_POT_DIR}${src_file_name/.po/.pot}
     # echo ${DST_DIR}${src_file_name}
-    msgmerge -v --previous -o ${DST_DIR}${src_file_name} ${SRC_DIR}${src_file_name} ${DST_POT_DIR}${src_file_name/.po/.pot}
+    msgmerge -v --previous -o "${DST_DIR%/}/${src_file_name}" "${SRC_DIR%/}/${src_file_name}" "${DST_POT_DIR%/}/${src_file_name/.po/.pot}"
 done
